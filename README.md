00b5b2e7bd1fb603e2535856d2a0377947235784
Pierre V. Villeneuve <pierre.villeneuve@gmail.com>
2017-04-29 15:49:04 -0700

    still

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # GoProHelper
 
-Simple Python API for getting stuff done with my GoPro Hero5 Black
+Simple Python API for getting stuff done with my GoPro Hero5 Black.
 
 A great deal of inspiration came from Konrad Iturbe's
 [GoPro wifi hack](https://github.com/KonradIT/goprowifihack) project on Github.  
---
+++ setup.py
@@ -0,0 +1,30 @@
+
+from __future__ import division, print_function, unicode_literals, absolute_import
+
+import setuptools
+
+"""
+PyPi Instructions:
+https://packaging.python.org/distributing/#uploading-your-project-to-pypi
+
+twine command-line tool:
+https://github.com/pypa/twine
+"""
+
+version = '2017.4.29'
+
+dependencies = []
+
+setuptools.setup(install_requires=dependencies,
+                 include_package_data=True,
+                 packages=setuptools.find_packages(),
+                 # zip_safe=False,
+
+                 name='gopro_helper',
+                 description='Simple Python API for getting stuff done with my GoPro Hero5 Black',
+                 author='Pierre V. Villeneuve',
+                 author_email='pierre.villeneuve@gmail.com',
+                 url='https://github.com/Who8MyLunch/GoProHelper',
+                 download_url='https://github.com/Who8MyLunch/GoProHelper/archive/{}.tar.gz'.format(version),
+                 version=version,
+                 keywords=['video', 'photo', 'gopro', 'api'])
